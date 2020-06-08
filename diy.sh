87833a9d0ace3da1c1f178ccb59e657ba7702269
ledewrt <61609258+ledewrt@users.noreply.github.com>
2020-06-08 20:16:51 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -106,7 +106,7 @@ sed -i 's/@IPV6 //g' package/*/*/*/netfilter.mk
 sed -i "s/askfirst/respawn/g" target/linux/x86/base-files/etc/inittab
 sed -i "/mediaurlbase/d" package/*/*/luci-theme*/root/etc/uci-defaults/*
 date=`date +%m.%d.%Y`
-#sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V %C by GaryPang'/g" package/base-files/files/etc/openwrt_release
+sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='LedeWrt Snapshot D%D %V %C From GaryPang'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/# REVISION:=x/REVISION:= $date/g" include/version.mk
 cp -f default-settings package/*/*/default-settings/files/zzz-default-settings
 if [ -n "$(ls -A "patches" 2>/dev/null)" ]; then
